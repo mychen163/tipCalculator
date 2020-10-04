99b0af5a3165924a118c53afb6277d11e4165d3d
M.y Chen <chen.mengyuan@gmail.com>
2020-10-03 23:40:36 -0700

    remove test comments

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -56,8 +56,6 @@ class ViewController: UIViewController,UITextFieldDelegate {
         //Calculate tip and total
         let tip = bill * tipPercentage
         let total = bill + tip
-        // this is a test to see git changes
-        //test 2
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLable.text = String(format: "$%.2f", total)
     }
